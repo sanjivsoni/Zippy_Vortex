b2961394bd8cd1a8795d8a2260c4fa9684c60734
sanjiv1994 <sanjiv1994@gmail.com>
2014-09-12 17:54:16 +0530

    Bluetooth Debugging with 90 Degree and Junction Detection Implimentations added

--- Zippy_Vortex.ino
+++ Zippy_Vortex.ino
@@ -8,11 +8,12 @@
 #define KD	80
 
 // MAX SPEED
-#define SPEED_MAX 4095
+#define SPEED_MAX 2048
 #define SPEED_CALIBRATE 786
+#define SPEED_TURN 1024
 
 // Enable for white line on black background
-#define WHITE_ON_BLACK 0
+#define WHITE_ON_BLACK 1
 
 // QTR 8RC SETUP
 #define NUMBER_OF_SENSORS 8
@@ -40,38 +41,42 @@ unsigned char sensorPins[] = { 14, 15, 16, 17, 18, 19, 20, 21 };
 unsigned short sensorValues[NUMBER_OF_SENSORS];
 
 unsigned char ROBOT_STATE = 0;
-//String bluetoothBuffer;
-//char bluetoothChar;
+
+#define BLUETOOTH Serial1
 
 // PID Variables
 float position_ = 0, proportional = 0, derivative = 0, integral = 0, lastProportional = 0;
 float control = 0;
 
 // Record the path
-String path;
+char path[100];
 unsigned int pathCounter = 0;
 
 // Initialize QTR8RC Sensor Array with sensor pins array
 // and number of sensors as second parameter
 QTRSensorsRC qtrRC(sensorPins, NUMBER_OF_SENSORS, TIMEOUT, EMITTER_PIN);
 
-void turn(char direction)
+void turn(char direction,unsigned int speed,unsigned short delayTime)
 {
 	switch (direction)
 	{
 	case 'L':
-		motorLeft.write(-SPEED_CALIBRATE);
-		motorRight.write(SPEED_CALIBRATE);
+		motorLeft.write(-speed);
+		motorRight.write(speed);
+		delay(delayTime);
 		break;
 
 	case 'R':
-		motorLeft.write(SPEED_CALIBRATE);
-		motorRight.write(-SPEED_CALIBRATE);
+		motorLeft.write(speed);
+		motorRight.write(-speed);
+		delay(delayTime);
 		break;
 
 	case 'S':
 		motorLeft.stop();
 		motorRight.stop();
+		DISABLE_STANDBY;
+
 		break;
 	}
 
@@ -83,19 +88,19 @@ void initializeBot()
 	for (unsigned int i = 0; i < 90; i++)
 	{
 		if (i == 0)
-			turn('L');
+			turn('L',SPEED_CALIBRATE,0);
 		if (i == 30)
-			turn('R');
+			turn('R', SPEED_CALIBRATE, 0);
 		if (i == 70)
-			turn('L');
+			turn('L', SPEED_CALIBRATE, 0);
 
 		// Emitters on
-		if (WHITE_ON_BLACK == 1 && EMITTER_ON == 1)
-			qtrRC.calibrate(QTR_EMITTERS_ON);
+		//if (WHITE_ON_BLACK == 1 && EMITTER_ON == 1)
+		//	qtrRC.calibrate(QTR_EMITTERS_ON);
 
 		// Emitters off
-		else
-			qtrRC.calibrate();
+		//else
+		qtrRC.calibrate();
 
 	}
 
@@ -132,7 +137,7 @@ void setup()
 {
 	if (DEBUG_MODE == 1)
 	{
-		Serial.begin(115200);
+		Serial.begin(9600);
 	}
 
 	//BLUETOOTH.begin(9600);
@@ -140,6 +145,7 @@ void setup()
 	pinMode(MOTOR_DRIVER_PIN_STANDBY, OUTPUT);
 	pinMode(13, OUTPUT);
 
+	//DISABLE_STANDBY;
 	ENABLE_STANDBY;
 
 	pinMode(BUZZER_PIN, OUTPUT);
@@ -156,6 +162,8 @@ void setup()
 	//12 bit width of analog write values
 	analogWriteResolution(12);
 
+	BLUETOOTH.begin(9600);
+
 	digitalWrite(13, HIGH);
 	BUZZER_ON;
 	initializeBot();
@@ -166,60 +174,83 @@ void setup()
 
 void runMappingMode()
 {
-	// All sensors on white
-	//while (sensorValues[0] < 200 && sensorValues[1] < 200 && sensorValues[2] < 200 && sensorValues[3] < 200 && sensorValues[4] < 200 && sensorValues[5] < 200 && sensorValues[6] < 200 && sensorValues[7] < 200)
-	//{
-	//	BUZZER_ON;
-	//	position_ = qtrRC.readLine(sensorValues, QTR_EMITTERS_ON, true);
 
-	//}
-	//if (position_ == 0 || position_ == 7000)
-	//	turn('L');
-	//else if (position_ > 2500 && position_ < 4500)
-	//{
-	//	motorLeft.stop();
-	//	motorRight.stop();
-	//}
+	if (sensorValues[1] < 200 && sensorValues[2] < 200 && sensorValues[3] < 200 && sensorValues[4] < 200 && sensorValues[5] < 200 && sensorValues[6] < 200)
+	{
+		while (sensorValues[1] < 200 && sensorValues[2] < 200 && sensorValues[3] < 200 && sensorValues[4] < 200 && sensorValues[5] < 200 && sensorValues[6] < 200)
+		{
+			BUZZER_ON;
+			position_ = qtrRC.readLine(sensorValues, QTR_EMITTERS_ON, true);
+		}
 
+		if (sensorValues[1] > 200 && sensorValues[2] > 200 && sensorValues[3] > 200 && sensorValues[4] > 200 && sensorValues[5] > 200 && sensorValues[6] > 200)
+		{
+			//Serial.print("90 Detected");
+			turn('L', SPEED_TURN, 100);
+			path[pathCounter++] = 'J';
+			BLUETOOTH.print(path[pathCounter - 1]);
+		}
+		else
+		{
+			turn('S', 0, 0);
+			path[pathCounter++] = 'S';
+			path[pathCounter] = '\0';
+			BLUETOOTH.println(path);
 
+		}	
+		BUZZER_OFF;
+	}
 
-	// 11110000 or 11100000 left 90degree turn
-	//if (sensorValues[0] > 750 && sensorValues[1] > 750 && sensorValues[2] > 750 && (sensorValues[3] > 750 || sensorValues[4] > 750))
-	//{
-	//	if (oneInchLine == 'B')
-	//	{
-	//		turn('L');
-	//		// B - left 
-	//		path[pathCounter++] = 'L';
-	//	}
-	//	//White Line Ahead
-	//	else if (oneInchLine == 'F')
-	//	{
-	//		turn('L');
-	//		// B - Left
-	//		path[pathCounter++] = 'B';
+	// Left 90 Turn
+	else if (sensorValues[0] < 200 && sensorValues[1] < 200 && sensorValues[2] < 200 && sensorValues[3] < 200 && sensorValues[4] < 200)
+	{
 
-	//	}
-	//}
+		while (sensorValues[0] < 200 && sensorValues[1] < 200 && sensorValues[2] < 200 && sensorValues[3] < 200 && sensorValues[4] < 200)
+		{
+			BUZZER_ON;
+			position_ = qtrRC.readLine(sensorValues, QTR_EMITTERS_ON, true);
+		}
+		if (sensorValues[1] > 200 && sensorValues[2] > 200 && sensorValues[3] > 200 && sensorValues[4] > 200 && sensorValues[5] > 200 && sensorValues[6] > 200)
+		{
+			//Serial.print("90 Detected");
+			turn('L',SPEED_TURN,100);
+			path[pathCounter++] = 'L';
+			BLUETOOTH.print(path[pathCounter - 1]);
+		}
+		else
+		{
+			turn('L', SPEED_TURN, 100);
+			path[pathCounter++] = 'J';
+			BLUETOOTH.print(path[pathCounter - 1]);
+		}
+		BUZZER_OFF;
+	}
 
-	//// 00001111 or 00000111 right 90degree turn
-	//else if (sensorValues[6] > 750 && sensorValues[7] > 750 && sensorValues[2] > 750 && (sensorValues[5] > 750 || sensorValues[4] > 750))
-	//{
-	//	//Nothing Ahead
-	//	if (oneInchLine == 'B')
-	//	{
-	//		turn('R');
-	//		path[pathCounter++] = 'J';
-	//	}
-	//	//White Line Ahead
-	//	else if (oneInchLine == 'F')
-	//	{
-	//		// A - Right
-	//		turn('A');
-	//		//Reached the end of MAPPPING_MODE
-	//	}
-	//}
+	// Right 90 Turn
+	else if (sensorValues[4] < 200 && sensorValues[5] < 200 && sensorValues[6] < 200 && sensorValues[7] < 200)
+	{
 
+		while (sensorValues[4] < 200 && sensorValues[5] < 200 && sensorValues[6] < 200 && sensorValues[7] < 200)
+		{
+			BUZZER_ON;
+			position_ = qtrRC.readLine(sensorValues, QTR_EMITTERS_ON, true);
+		}
+		if (sensorValues[1] > 200 && sensorValues[2] > 200 && sensorValues[3] > 200 && sensorValues[4] > 200 && sensorValues[5] > 200 && sensorValues[6] > 200)
+		{
+			//Serial.print("90 Detected");
+			turn('R', SPEED_TURN, 100);
+			path[pathCounter++] = 'R';
+			BLUETOOTH.print(path[pathCounter - 1]);
+		}
+		else
+		{
+			turn('R', SPEED_TURN, 100);
+			path[pathCounter++] = 'J';
+			BLUETOOTH.print(path[pathCounter - 1]);
+
+		}
+		BUZZER_OFF;
+	}
 }
 
 
@@ -233,7 +264,7 @@ void loop()
 	else
 		position_ = qtrRC.readLine(sensorValues);
 
-	//runMappingMode();
+	runMappingMode();
 
 	if (DEBUG_MODE == 1)
 		showSensorValues();
@@ -246,7 +277,7 @@ void loop()
 
 	control = proportional *KP + integral*KI + derivative*KD;
 
-	if (DEBUG_MODE == 1)
+	if (DEBUG_MODE == 2)
 	{
 		Serial.print("Control  ");
 		Serial.print(control);
